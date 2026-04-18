e69672b804b534dc2a937c585bdbd24f8c6ba228
MaleeshaVishvani <vishvaniliyanage1111@gmail.com>
2026-04-18 15:42:07 +0530

    lab04 q3 second child class

--- OOP_JAVA1/src/main/java/lab04main.java
+++ OOP_JAVA1/src/main/java/lab04main.java
@@ -10,7 +10,14 @@
 public class lab04main {
     public static void main(String[] args) {
         Lecturer lec1 = new Lecturer("Dr. Perera", "LEC-001", "Computing", 4, 15000.0);
+       
+        LabAssistant la1 = new LabAssistant("Kasun Silva", "LA-001", "Computing", 80, 500.0);
         lec1.displayLecturerDetails();
         System.out.println("Monthly Payment: " + lec1.calculateMonthlyPayment());
+
+        System.out.println();
+
+        la1.displayLabAssistantDetails();
+        System.out.println("Monthly Payment: " + la1.calculateMonthlyPayment());
     }
 }
